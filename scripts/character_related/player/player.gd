ac0212b6045c77651a25a42181e593ed3edf053d
Ryan Hinds <fossryanh@gmail.com>
2024-05-02 12:15:09 -0600

    changed out assets and redid animations

--- scripts/character_related/player/player.gd
+++ scripts/character_related/player/player.gd
@@ -9,15 +9,15 @@ extends CharacterBody2D
 var jump_count = 0
 
 func _physics_process(_delta: float) -> void:
-    var horizontal_input = Input.get_axis("MoveLeft", "MoveRight")
+	var horizontal_input = Input.get_axis("MoveLeft", "MoveRight")
 
-    velocity.x = horizontal_input * movement_speed
-    velocity.y += gravity
+	velocity.x = horizontal_input * movement_speed
+	velocity.y += gravity
 
-    if Input.is_action_just_pressed("Jump") and jump_count <= jump_force.size():
-        velocity.y = -jump_force[jump_count]
-        jump_count += 1
-    elif is_on_floor():
-        jump_count = 0
-    
-    move_and_slide()
+	if Input.is_action_just_pressed("Jump") and jump_count < jump_force.size():
+		velocity.y = -jump_force[jump_count]
+		jump_count += 1
+	elif is_on_floor():
+		jump_count = 0
+	
+	move_and_slide()
